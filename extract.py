77e1cdac841a7adf2e4fa9e335fc48b5f9c55cbb
Ubuntu <ubuntu@ip-172-31-41-89.us-east-2.compute.internal>
2019-12-13 20:32:36 +0000

    Module which generates an access token and downloads project and task data

---
+++ extract.py
@@ -0,0 +1,75 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+from pandas.io.json import json_normalize
+
+
+SELECT_PARAMS = [
+    'ID',
+    'BaseName',
+    'Sponsor',
+    'ReportStatus1',
+    'SPW',
+    'WFProgress',
+    'DateInitialStage',
+    'DateInProgressRFI',
+    'DateInProgressDrafting',
+    'DateSponsorReview',
+    'DateFinalizeProject',
+    'DateCompleted1',
+    'ProjectLead/Title',
+    'ReportType',
+    'ProgramType',
+    'LinkToOpportunity',
+    'First_x0020_Draft_x003f_',
+    'QC',
+    'Editor0',
+    'DraftSentDate',
+    'Final'
+]
+
+
+
+def auth_token():
+    load_dotenv()
+    url = 'https://accounts.accesscontrol.windows.net/' + os.getenv('SP_TENANT_ID') + '/tokens/OAuth/2'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    body = {
+        'grant_type': 'client_credentials',
+        'client_id': os.getenv('SP_CLIENT_ID') + '@' + os.getenv('SP_TENANT_ID'),
+        'client_secret': os.getenv('SP_CLIENT_SECRET'),
+        'resource': os.getenv('SP_RESOURCE') + '/' + os.getenv('SP_DOMAIN') + '@' + os.getenv('SP_TENANT_ID')
+    }
+
+    return requests.get(url=url, headers=headers, data=body).json()['access_token']
+
+
+def projects_url():
+    load_dotenv()
+    return os.getenv('SP_PROJECTS_URL')
+
+
+def tasks_url():
+    load_dotenv()
+    return os.getenv('SP_TASKS_URL')
+
+
+def headers():
+    return {'Accept':'application/json;odata=verbose',
+           'Authorization':'Bearer ' + auth_token()}
+
+
+def download_data(url, headers):
+    data = requests.get(url=url, headers=headers).json()
+    return json_normalize(data['d']['results'])
+
+
+def extract_projects():
+    return download_data(projects_url(), headers())
+
+
+def extract_tasks():
+    return download_data(tasks_url(), headers())
+
+print(extract_tasks())
